cf8044c2d40fd09d62a1ea6935f034032e0ee1ff
Clayton Jones <claytonjonesLMP@gmail.com>
2021-04-15 12:54:33 -0700

    created csv reader class

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
-import fs from 'fs';
+// import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
+
+// reader.read();
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') manUnitedWins++;
-  else if (match[2] === 'Man United' && match[5] === 'A') manUnitedWins++;
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin)
+    manUnitedWins++;
+  else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
+    manUnitedWins++;
 }
 
 console.log(`Man United won ${manUnitedWins} games`);
