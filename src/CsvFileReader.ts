a301d377458fada749f0676b90c771c3eb9fe85c
Clayton Jones <claytonjonesLMP@gmail.com>
2021-04-15 13:21:02 -0700

    refactored csv file reader to include tuple and type assertions

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,13 +1,17 @@
 import fs from 'fs';
+import { parseDate } from './utils';
+import { MatchResult } from './MatchResult';
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
 
 export class CsvFileReader {
-  data: string[][] = [];
+  data: MatchData[] = [];
 
   constructor(public filename: string) {
     this.read();
   }
 
-  read(): void {
+  private read(): void {
     this.data = fs
       .readFileSync(this.filename, {
         encoding: 'utf-8',
@@ -15,6 +19,19 @@ export class CsvFileReader {
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
-      });
+      })
+      .map(
+        (row: string[]): MatchData => {
+          return [
+            parseDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult,
+            row[6],
+          ];
+        }
+      );
   }
 }
